84f879a45f6f855da79b632b98ccd299341a45c5
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-07-31 22:05:42 +0530

    init

---
+++ Chat-fe/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  function sendMessage() {
+    
+  }
+
+  
+
+  return <div className = "flex justify-center items-center gap-2">
+    <input type="text" placeholder = "......Type" />
+    <button onClick = {sendMessage}>Send</button>
+  </div>
+}
+
+export default App
