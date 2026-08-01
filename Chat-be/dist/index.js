a484bbedcf86c236ec82ffa0e0f2fdf9a332ca9c
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-08-01 22:10:42 +0530

    chat room implementation

--- Chat-be/dist/index.js
+++ Chat-be/dist/index.js
@@ -1,12 +1,23 @@
 import WebSocket, { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-const allSocket = [];
+let allSocket = [];
 wss.on("connection", (socket) => {
-    allSocket.push(socket);
     socket.on("message", (e) => {
-        for (let i = 0; i < allSocket.length; i++) {
-            const s = allSocket[i];
-            s?.send(e.toString());
+        // @ts-ignore
+        const parsedData = JSON.parse(e);
+        if (parsedData.type == "Join") {
+            allSocket.push({
+                socket: socket,
+                roomId: parsedData.payload.roomId
+            });
+        }
+        if (parsedData.type == "chat") {
+            const userRoom = allSocket.find((x) => x.socket == socket)?.roomId;
+            for (let i = 0; i < allSocket.length; i++) {
+                if (allSocket[i]?.roomId == userRoom) {
+                    allSocket[i]?.socket.send(parsedData.payload.message);
+                }
+            }
         }
     });
 });
